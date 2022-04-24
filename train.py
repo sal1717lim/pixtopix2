3a84bb05707f955241577395b3e51b984734ed28
SALIM <saissi1701@gmail.com>
2022-04-24 10:33:07 +0100

    n'oublie pas les path

--- train.py
+++ train.py
@@ -55,8 +55,46 @@ def train_fn(
             loop.set_postfix(
                 D_real=torch.sigmoid(D_real).mean().item(),
                 D_fake=torch.sigmoid(D_fake).mean().item(),
+                L1    =L1.item()
             )
+def test_fn(
+    disc, gen, loader, opt_disc, opt_gen, l1_loss, bce, g_scaler, d_scaler,
+):
+    loop = tqdm(loader, leave=True)
+    with torch.no_grad():
+     resultat=[]
+     for idx, (x, y) in enumerate(loop):
+        x = x.to(config.DEVICE)
+        y = y.to(config.DEVICE)
 
+        # Train Discriminator
+        with torch.cuda.amp.autocast():
+            y_fake = gen(x)
+            D_real = disc(x, y)
+            D_real_loss = bce(D_real, torch.ones_like(D_real))
+            D_fake = disc(x, y_fake.detach())
+            D_fake_loss = bce(D_fake, torch.zeros_like(D_fake))
+            D_loss = (D_real_loss + D_fake_loss) / 2
+
+
+
+        # Train generator
+        with torch.cuda.amp.autocast():
+            D_fake = disc(x, y_fake)
+            G_fake_loss = bce(D_fake, torch.ones_like(D_fake))
+            L1 = l1_loss(y_fake, y) * config.L1_LAMBDA
+            G_loss = G_fake_loss + L1
+            resultat.append(L1.item())
+
+
+
+        if idx % 10 == 0:
+            loop.set_postfix(
+                D_real=torch.sigmoid(D_real).mean().item(),
+                D_fake=torch.sigmoid(D_fake).mean().item(),
+                L1    =L1.item()
+            )
+    return torch.tensor(resultat).mean()
 
 def main():
     #instancing the models
@@ -88,6 +126,13 @@ def main():
         shuffle=True,
         num_workers=config.NUM_WORKERS,
     )
+    test_dataset = Kaiset(path=config.TRAIN_DIR,train=False, Listset=config.TRAIN_LIST)
+    test_loader = DataLoader(
+        test_dataset,
+        batch_size=config.BATCH_SIZE,
+        shuffle=True,
+        num_workers=config.NUM_WORKERS,
+    )
     #enabling MultiPrecision Mode, the optimise performance
     g_scaler = torch.cuda.amp.GradScaler()
     d_scaler = torch.cuda.amp.GradScaler()
@@ -95,13 +140,16 @@ def main():
     #evauation data loading
     val_dataset = Kaiset(path=config.VAL_DIR, Listset=config.TEST_LIST, shuffle=True)
     val_loader = DataLoader(val_dataset, batch_size=config.EVAL_BATCH_SIZE, shuffle=False)
-
+    best=10000000
+    resultat=1
     for epoch in range(config.NUM_EPOCHS):
         train_fn(
-            disc, gen, train_loader, opt_disc, opt_gen, L1_LOSS, BCE, g_scaler, d_scaler,
+           disc, gen, train_loader, opt_disc, opt_gen, L1_LOSS, BCE, g_scaler, d_scaler,
         )
-
-        if config.SAVE_MODEL and epoch % 5 == 0:
+        resultat=test_fn(disc, gen, test_loader, opt_disc, opt_gen, L1_LOSS, BCE, g_scaler, d_scaler,)
+        if best>resultat:
+            best=resultat
+            print("improvement of the loss from {} to {}".format(best,resultat))
             save_checkpoint(gen, opt_gen, epoch, filename=config.CHECKPOINT_GEN)
             save_checkpoint(disc, opt_disc, epoch, filename=config.CHECKPOINT_DISC)
 
